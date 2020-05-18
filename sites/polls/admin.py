584008e8f7853cad2d4e132168a19f164a379b4c
sachinupadhyay991 <33253002+sachinupadhyay991@users.noreply.github.com>
2020-05-18 19:10:43 +0530

    Add files via upload

---
+++ sites/polls/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+
+# Register your models here.
+admin.site.register(Global_Indices)
+admin.site.register(MAS_BSE)
+admin.site.register(MAS_NSE)
+admin.site.register(FD_CASH)
+admin.site.register(FD_FII)
+admin.site.register(FD_FandO)
+admin.site.register(FD_MF_SEBI)
+admin.site.register(Indian_Indices)
+admin.site.register(graph)
